47568088a3bfcd0d34bc4752012b2a46434de9ea
Rhys Evans <EvansRM17@cardiff.ac.uk>
2023-11-23 13:20:34 +0000

    Successfully updated current webpages to incorporate controller GetMapping for URL address usage

--- src/main/java/Team5/SmartTowns/Webpages/WebpageController.java
+++ src/main/java/Team5/SmartTowns/Webpages/WebpageController.java
@@ -3,27 +3,26 @@ package Team5.SmartTowns.Webpages;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
+import org.springframework.stereotype.Controller;
 @Controller
 public class WebpageController {
 
 
-    @GetMapping()
-    public ModelAndView getXWebPage("/html page here"){
-        ModelAndView modelAndView = new ModelAndView("Desired ViewName");
+    @GetMapping("/Caerleon")
+    public ModelAndView getCaerleonPage(){
+        ModelAndView modelAndView = new ModelAndView("towns/caerleon");
         return modelAndView;
     }
 
-    @GetMapping()
-    public ModelAndView getYWebPage("/html page here"){
-        ModelAndView modelAndView = new ModelAndView("Desired ViewName");
+    @GetMapping("/Caerphilly")
+    public ModelAndView getCaerphillyPage(){
+        ModelAndView modelAndView = new ModelAndView("towns/caerphilly");
         return modelAndView;
     }
 
-    @GetMapping()
-    public ModelAndView getZWebPage("/html page here"){
-        ModelAndView modelAndView = new ModelAndView("Desired ViewName");
+    @GetMapping("/Risca")
+    public ModelAndView getRiscaPage(){
+        ModelAndView modelAndView = new ModelAndView("towns/risca");
         return modelAndView;
     }
 
