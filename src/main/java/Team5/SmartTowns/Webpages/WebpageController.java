c9467509afe7005e9b6e2749a1eff3618e0efe35
Rhys Evans <EvansRM17@cardiff.ac.uk>
2023-11-23 11:36:16 +0000

    Renamed webpage controller class

--- src/main/java/Team5/SmartTowns/Webpages/WebpageController.java
+++ src/main/java/Team5/SmartTowns/Webpages/WebpageController.java
@@ -1,4 +1,4 @@
-package Team5.SmartTowns.WebPages;
+package Team5.SmartTowns.Webpages;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 @Controller
-public class WebPageController {
+public class WebpageController {
 
 
     @GetMapping()
     public ModelAndView getXWebPage("/html page here"){
         ModelAndView modelAndView = new ModelAndView("Desired ViewName");
-return modelAndView;
+        return modelAndView;
     }
 
     @GetMapping()
