9cbd4208b05053a2b3927b3f4a2775c569204787
Gabriel Copat <copatg@cardiff.ac.uk>
2023-11-28 16:46:29 +0000

    Changed method of updaing trail data to a GET request.
    Trails now load as nescessary instead of having all trails loaded and then hidding/unhiding as needed

--- src/main/java/Team5/SmartTowns/trails/TrailsController.java
+++ src/main/java/Team5/SmartTowns/trails/TrailsController.java
@@ -4,9 +4,13 @@ package Team5.SmartTowns.trails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TrailsController {
@@ -20,5 +24,15 @@ public class TrailsController {
     public String sendHtmlFragment(Model map) {
         map.addAttribute("foo", "bar");
         return "allTrails/allTrails";
+    }
+
+    @GetMapping("/allTrails/{id}")
+    public ModelAndView getResultBySearchKey(@PathVariable int id)
+    {
+        List<Trail> trailList= Trail.trails;//results from db
+        ModelAndView mv= new ModelAndView("fragments/allTrailsFrags :: trailSection");
+        mv.addObject("searchList", trailList.get(id-1));
+
+        return mv;
     }
 }
