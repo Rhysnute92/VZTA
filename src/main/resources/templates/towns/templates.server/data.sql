3d96591a7ee82d05bcca91f22e23ca52b4358c4d
Rhys Nute <nuterd@cardiff.ac.uk>
2023-11-29 18:11:33 +0000

    code debugging

--- src/main/resources/templates/towns/templates.server/data.sql
+++
@@ -1,4 +0,0 @@
-insert into users (userId, title, firstName, lastName, emailAddress) values (1, Mrs, Hannah, English, hannah.english@gmail.com);
-insert into users (userId, title, firstName, lastName, emailAddress) values (2, Mr, Tom, Harper, tom.harper2@gmail.com);
-insert into users (userId, title, firstName, lastName, emailAddress) values (3, Mrs, Sarah, Burton, sarahburton@gmail.com);
-insert into users (userId, title, firstName, lastName, emailAddress) values (4, Mr, Nigel, Hopkins, nigelahopkins@gmail.com);
--- src/main/resources/templates/towns/templates.server/schema.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists users
-(
-    userId    bigint auto_increment primary key,
-    title     varchar(10),
-    firstName varchar(64),
-    lastName  varchar(64),
-    emailAddress varchar(128)
-    ) engine =InnoDB;
