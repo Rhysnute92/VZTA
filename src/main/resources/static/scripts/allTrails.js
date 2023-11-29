04d3bd919b1d9cff8c6bff923b3387ad2ad2a318
Gabriel Copat <copatg@cardiff.ac.uk>
2023-11-29 11:35:41 +0000

    Moved JavaScript to it's own file

--- src/main/resources/static/scripts/allTrails.js
+++ src/main/resources/static/scripts/allTrails.js
@@ -1,2 +1,18 @@
-
-/* Functions taken from https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css*/
+function updateOutputTrail(id) {
+    /* Updates the trail being shown on screen to the one requested by ID */
+    $.get("/allTrails/" + id).done(function (fragment) {
+        $("#trailInfoBox").html(fragment)
+    });
+    updateSelectedTrail(id);
+}
+function updateSelectedTrail(id) {
+    /* Updates the trail bar, so that it highlights the selected trail */
+    let list = document.getElementsByClassName('trailsImages')
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].classList.contains('selected')) {
+            list[i].classList.remove('selected')
+            break
+        }
+    }
+    document.getElementById("img" + id).classList.add("selected")
+}
