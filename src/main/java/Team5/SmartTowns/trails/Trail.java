c9f2c34d0358a828a23ca73831ca7a6f2c3f7863
Gabriel Copat <copatg@cardiff.ac.uk>
2023-11-29 16:51:39 +0000

    Minor updates to CSS

--- src/main/java/Team5/SmartTowns/trails/Trail.java
+++ src/main/java/Team5/SmartTowns/trails/Trail.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 public class Trail {
     public static List<Trail> trails = List.of(
-            new Trail(1,"Trail1", "This is trail one"),
+            new Trail(1,"Caerphilly Castle Trail", "Take a stroll through the grounds of one of Europe's finest historic buildings and you will see stunning views of the castle and the lakes. The route of the trail is marked with eight special circular markers, which depict various fascinating historical facts relating to the castle. Pupils from Ysgol Gynradd Gymraeg Caerffili, Plasyfelin Junior School, Ysgol Y Castell and Twyn Primary worked with the artist to come up with the different designs. You do not need to pay to go in the castle to complete this trail. This Trail is fairly short at 1.5 miles and very suitable for wheelchairs and pushchairs as the route stays mostly on well-marked and ramped paths with just a couple of short diversions onto grassed areas."),
             new Trail(2,"Trail2", "This is trail two"),
             new Trail(3,"Trail3", "This is trail three"),
             new Trail(4,"Trail4", "This is trail four"),
