e9129acb71bf77a39c88a57fddc19090533b8759
Rhys Nute <nuterd@cardiff.ac.uk>
2023-11-30 22:02:16 +0000

    tidied schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,18 @@
 drop table if exists trail;
-create table if not exists trail(
-    trailID bigint auto_increment primary key,
-    name varchar(100)
-) engine=InnoDB;
-drop table if exists locations;
-create table if not exists locations(
-locationID bigint auto_increment primary key,
-name varchar(100)
-) engine=InnoDB;
-drop table if exists users;
-create table if not exists users(
-    userID bigint auto_increment primary key,
-    name varchar(100)
-) engine=InnoDB;
+    create table if not exists trail
+    (
+        trailID bigint auto_increment primary key,
+        name varchar
+    )   engine=InnoDB;
+            drop table if exists locations;
+            create table if not exists locations
+            (
+                locationID bigint auto_increment primary key,
+                name varchar
+            )   engine=InnoDB;
+                    drop table if exists users;
+                    create table if not exists users
+                    (
+                        userID bigint auto_increment primary key,
+                        name varchar
+                    ) engine=InnoDB;
