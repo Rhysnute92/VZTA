28a8671b127b409442118f8562580608d94a2a4d
Rhys Evans <EvansRM17@cardiff.ac.uk>
2023-11-30 11:45:17 +0000

    added initial java files within main.java.Team5.SmartTowns.Towns to follow more dynamic town list in HomePage.html

---
+++ src/main/java/Team5/SmartTowns/Towns/towns.java
@@ -0,0 +1,52 @@
+package Team5.SmartTowns.Towns;
+
+import lombok.Data;
+
+@Data
+public class towns {
+    private String name;
+    private Integer id;
+    private int trailNumber;
+    private int trailProgress;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getTrailNumber() {
+        return trailNumber;
+    }
+
+    public void setTrailNumber(int trailNumber) {
+        this.trailNumber = trailNumber;
+    }
+
+    public int getTrailProgress() {
+        return trailProgress;
+    }
+
+    public void setTrailProgress(int trailProgress) {
+        this.trailProgress = trailProgress;
+    }
+
+
+    public towns(String name, Integer id, int trailNumber, int trailProgress) {
+        this.name = name;
+        this.id = id;
+        this.trailNumber = trailNumber;
+        this.trailProgress = trailProgress;
+    }
+}
