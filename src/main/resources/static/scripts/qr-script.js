19af09c9a0055c3076cb6d5215feef65567aeefe
Rhys Nute <nuterd@cardiff.ac.uk>
2023-12-03 19:01:00 +0000

    QR updates

--- src/main/resources/static/scripts/qr-script.js
+++ src/main/resources/static/scripts/qr-script.js
@@ -14,13 +14,13 @@ function domReady(fn){
 }
 domReady(function (){
     //if QR code found
-    function onQRSuccess(decodeText, decodeResult){
+    function onScanSuccess(decodeText, decodeResult){
         alert("You have collected: " + decodeText, decodeResult);
     }
-let htmlscanner = new Html5QrcodeScanner(
+    let htmlscanner = new Html5QrcodeScanner(
     "qr-code-reader",
-    { fps: 10, qrbos: 250}
-)
-    htmlscanner.render(onQRSuccess);
+    { fps: 20, qrbos: 250}
+    );
+    htmlscanner.render(onScanSuccess);
 });
 
