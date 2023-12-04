a9767edbd503ef4f02a640cc6469fbdc422401b7
Rhys Nute <nuterd@cardiff.ac.uk>
2023-12-04 14:19:37 +0000

    Finished part one

--- src/main/resources/static/scripts/qr-script.js
+++ src/main/resources/static/scripts/qr-script.js
@@ -1,5 +1,5 @@
 //qr-script file - R Nute
-
+//Modified from (https://www.geeksforgeeks.org/create-a-qr-code-scanner-or-reader-in-html-css-javascript/)
 function domReady(fn){
     if (
         document.readyState === "complete" ||
@@ -14,6 +14,9 @@ domReady(function (message){
     //if QR code found
     function onScanSuccess(decodeText, decodeResult){
         alert("You have collected: " + decodeText, decodeResult);
+        // Open the result, what methods are available through the HTML5 Qr scanner node package?
+        // Record the result, see above.
+        // Get user and tie to user account, intergrate with database, retrieving and storing under user info.
     }
     let htmlscanner = new Html5QrcodeScanner(
     "qr-code-reader",
