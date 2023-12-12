b278ab25b8e3e641ac84e3cedcfafcb106da0466
Connor <brockcc1@cardiff.ac.uk>
2023-12-12 01:59:56 +0000

    Refactored QRScanner code, added a controller to direct to the QRScanner and created a JS button to send the user to the QR url.

--- src/main/java/Team5/SmartTowns/dragonstale/DragonsTaleController.java
+++ src/main/java/Team5/SmartTowns/dragonstale/DragonsTaleController.java
@@ -3,9 +3,12 @@ package Team5.SmartTowns.dragonstale;
 import Team5.SmartTowns.landmarks.Landmarks;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 import static Team5.SmartTowns.dragonstale.DragonsTale.landmarksDragonstrail;
 
@@ -13,14 +16,32 @@ import static Team5.SmartTowns.dragonstale.DragonsTale.landmarksDragonstrail;
 
 @Controller
 public class DragonsTaleController {
+    ModelAndView modelAndView;
 
     @GetMapping("/dragonstale")
     public ModelAndView getDragonsTale(){
         List<Landmarks> landmarksList = landmarksDragonstrail;
-        ModelAndView modelAndView = new ModelAndView("/dragonstale/index");
+        modelAndView = new ModelAndView("/dragonstale/index");
         modelAndView.addObject("landmarksList", landmarksList);
         return modelAndView;
     }
+
+    @RequestMapping ("/QRScan") //In here, we could use trailID as a string variable and use it to track what trail the user clicked from.
+    public ModelAndView getQRScanner(){
+        modelAndView = new ModelAndView("qrCodeScanner/qr-scanner");
+        //Can we extract the pathvariable in a JS function?
+        return modelAndView;
+    }
+
+//    @GetMapping("/{trailID}/QRScan") //In here, we could use trailID as a string variable and use it to track what trail the user clicked from.
+//    public ModelAndView getQRScanner(@PathVariable Optional<Integer> trailID){
+//        ModelAndView modelAndView = new ModelAndView("/dragonstale/index");
+//        //Can we extract the pathvariable in a JS function?
+//        return modelAndView;
+//    }
+
+
+
 
 //    @GetMapping("dragonstale/{qrCode}/{id}")
 //    public String qrCodeCheck(@PathVariable Optional<String> qrCode, @PathVariable Optional<Integer> id){
---
+++ src/main/resources/static/scripts/DTscript.js
@@ -0,0 +1,3 @@
+document.getElementById("qrCodeScanner").addEventListener("click", function (){
+    window.location.href = "http://localhost:8080/QRScan";
+})
