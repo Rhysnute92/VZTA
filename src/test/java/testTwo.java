94ea2461bdd7f43ee92300c00bf7a3d9d2ba61e2
Rhys Evans <EvansRM17@cardiff.ac.uk>
2023-12-12 10:42:17 +0000

    cleared cache, seems to fix xsome issues with Junit testing

--- src/test/java/testTwo.java
+++ src/test/java/testTwo.java
@@ -1,7 +1,7 @@
 //package Team5.SmartTowns.data;
 
-import  org.junit.platform.commons.util.Preconditions;
-
+//import  org.junit.platform.commons.util.Preconditions;
+//
 import Team5.SmartTowns.data.Location;
 import Team5.SmartTowns.data.LocationRepository;
 import Team5.SmartTowns.data.LocationRepositoryJDBC;
@@ -15,34 +15,35 @@ import org.springframework.jdbc.core.RowMapper;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-@SpringBootTest
+//@SpringBootTest
 public class testTwo {
-//
-//private JdbcTemplate jdbcTemplate;
-//private LocationRepositoryJDBC locationRepositoryJDBC;
-//
-//private RowMapper<Location> locationMapper;
-//
-//@Autowired
-//    LocationRepository locationRepository;
-//
-//
-//@Autowired
-//    public void LocationRepositoryJDBC(JdbcTemplate jdbc){
-//    this.jdbcTemplate = jdbc;
-//    locationRepositoryJDBC = new LocationRepositoryJDBC(jdbc);
-//}
-//
-//
-//@BeforeAll
-//void setUp(){
-//    locationRepository.getAllLocation();
 
-//}
+private JdbcTemplate jdbcTemplate;
+private LocationRepositoryJDBC locationRepositoryJDBC;
+
+private RowMapper<Location> locationMapper;
+
+@Autowired
+private LocationRepository locationRepository;
+
+
+@Autowired
+    public void LocationRepositoryJDBC(JdbcTemplate jdbc){
+    this.jdbcTemplate = jdbc;
+    locationRepositoryJDBC = new LocationRepositoryJDBC(jdbc);
+}
+
+
+@BeforeAll
+public static void setUp(){
+    locationRepository = new LocationRepositoryJDBC.getAllLocation();
+// locationRepository = (LocationRepositoryJDBC) locationRepository.getAllLocation();
+}
 
 @Test
 public void test(){
-   assertEquals(1,1);
+    int aa=1;
+   assertEquals(1,aa);
 
     }
 
