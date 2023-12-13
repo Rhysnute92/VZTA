aba00a33c7fc2193fcbd67dd3f3b231b8a7bb362
Rhys Evans <EvansRM17@cardiff.ac.uk>
2023-12-13 14:44:12 +0000

    Updated test users name  to allow test to run a one without same user insertion conflicts

--- src/test/java/Team5/SmartTowns/testUsers.java
+++ src/test/java/Team5/SmartTowns/testUsers.java
@@ -43,7 +43,7 @@ public class testUsers {
     @Test // test if new users and inserted users can be found
     public void doesUserExistTest(){
         Boolean insertedUserFoundByEmail = userRepository.doesUserExist("Kevin@Gmail.com");
-        NewUser newuser = new NewUser("Meow","Woof","Cat@Dogs.com");
+        NewUser newuser = new NewUser("MeowMeow","WoofMeow","CatMeow@Dogs.com");
         Boolean newUser = userRepository.addUser(newuser.getName(), newuser.getEmail(), newuser.getPassword());
         Boolean newUserFoundByEmail = userRepository.doesUserExist(newuser.getEmail());
         int compareTwoSearches = Boolean.compare(insertedUserFoundByEmail, newUserFoundByEmail);
@@ -54,7 +54,7 @@ public class testUsers {
 
     @Test
     public void canUsersUnlockStickersTest(){
-        NewUser newuser = new NewUser("Meow","Woof","Cat@Dogs.com");
+        NewUser newuser = new NewUser("MeowMeowMeow","WoofMeowMeow","CatMeowMeow@Dogs.com");
         Boolean newUser = userRepository.addUser(newuser.getName(), newuser.getEmail(), newuser.getPassword());
         Boolean doesStickerUnlock = userRepository.unlockSticker(newuser.getName(),2,2);
         System.out.println(doesStickerUnlock);
@@ -62,7 +62,7 @@ public class testUsers {
     }
     @Test
     public void canUsersUnlockStickersAndViewThemTest(){
-        NewUser newuser = new NewUser("Meow","Woof","Cat@Dogs.com");
+        NewUser newuser = new NewUser("MeowMeowMeowMeow","WoofMeowMeowMeow","CatMeowMeowMeow@Dogs.com");
         NewUser newuserTwo = new NewUser("Jumper","Baa","Sheep@Wool.com");
         Boolean newUser = userRepository.addUser(newuser.getName(), newuser.getEmail(), newuser.getPassword());
         Boolean newUserTwo = userRepository.addUser(newuserTwo.getName(), newuserTwo.getEmail(), newuserTwo.getPassword());
@@ -75,5 +75,5 @@ public class testUsers {
 
     }
 
-    
+
 }
