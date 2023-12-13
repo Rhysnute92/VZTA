1a795df1972a11342f812d40675f497fa1bd982f
Gabriel Copat <copatg@cardiff.ac.uk>
2023-12-13 16:45:58 +0000

    More css, also security fix

--- src/main/java/Team5/SmartTowns/security/SecurityConfiguration.java
+++ src/main/java/Team5/SmartTowns/security/SecurityConfiguration.java
@@ -2,9 +2,11 @@ package Team5.SmartTowns.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -24,9 +26,11 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/user/**", "/userProfile").authenticated()
                         .anyRequest().permitAll()
+
                 )
                 .formLogin((login) -> login
                         .loginPage("/login").permitAll()
--- src/test/java/Team5/SmartTowns/DatasourceConfig.java
+++ src/test/java/Team5/SmartTowns/DatasourceConfig.java
@@ -5,17 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-
 @Configuration
-public class DatasourceConfig {
-        @Bean
-        public DataSource dataSource() {
-            DriverManagerDataSource dataSource = new DriverManagerDataSource();
-            dataSource.setUrl("jdbc:mariadb://localhost:3306/test_towns");
-            dataSource.setUsername("root");
-            dataSource.setPassword("comsc");
+public class DataSourceConfig {
 
-            return dataSource;
-        }
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/test_towns");
+        dataSource.setUsername("root");
+        dataSource.setPassword("comsc");
+        return dataSource;
+    }
+
 }
-
